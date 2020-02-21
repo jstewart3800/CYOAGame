95a0ed4e3b0ee09fd6ea5a289fbdea268f9226e4
jstewart3800 <56611713+jstewart3800@users.noreply.github.com>
2020-02-20 16:01:34 -0800

    Interface, game.ts (nextScene fixed)

---
+++ CYOAApp/src/app/interfaces/scene.ts
@@ -0,0 +1,11 @@
+export interface Scene {
+    id: number
+    sceneText: string
+    choice1: string
+    choice2: string
+    choice3: string
+    result1: number
+    result2: number
+    result3: number
+    ending: boolean
+}
--- CYOAApp/src/app/pages/game/game.page.ts
+++ CYOAApp/src/app/pages/game/game.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SceneService } from 'src/app/services/scene.service';
+import { Scene } from 'src/app/interfaces/scene';
 
 @Component({
   selector: 'app-game',
@@ -12,17 +13,25 @@ export class GamePage implements OnInit {
   // Might need some tweaking
   constructor(private sService: SceneService) {
     sService.getScenes().subscribe(
-      x => {console.log(x.feed.entry)
-      for (let sceneInQuestion of x.feed.entry) {
-        let nextScene = {
-          id: sceneInQuestion.gsx$id.$t,
-          sceneText: sceneInQuestion.gsx$scene.$t
-        };
-        this.myScenes.push(nextScene);
+      x => {
+        console.log(x.feed.entry)
+        for (let sceneInQuestion of x.feed.entry) {
+          let nextScene: Scene = {
+            id: sceneInQuestion.gsx$id.$t,
+            sceneText: sceneInQuestion.gsx$scene.$t,
+            choice1: sceneInQuestion.gsx$choice1.$t,
+            choice2: sceneInQuestion.gsx$choice2.$t,
+            choice3: sceneInQuestion.gsx$choice3.$t,
+            result1: sceneInQuestion.gsx$result1.$t,
+            result2: sceneInQuestion.gsx$result2.$t,
+            result3: sceneInQuestion.gsx$result3.$t,
+            ending: sceneInQuestion.gsx$ending.$t
+          };
+          this.myScenes.push(nextScene);
+        }
         console.log(this.myScenes);
-      }
       });
-   }
+  }
 
   ngOnInit() {
   }
